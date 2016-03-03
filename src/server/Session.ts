47b0c815c1e9708302cde2355b3415d6e7cefb10
fischerman <privat@bjorn-fischer.de>
2016-03-03 11:35:07 +0100

    Implement basic SessionManager

---
+++ src/server/Session.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../typings/main.d.ts" />
+import {Project} from '../common/project.ts'
+import {MipsRunner} from './mipsRunner'
+import {Gcc} from './gcc'
+import path = require('path');
+import cp = require('child_process');
+
+export class Session {
+	public hookSocket(socket: SocketIO.Socket) {
+		socket.on('run', function(project: Project) {
+			var gcc: Gcc = new Gcc(project);
+			gcc.compile((err, stdout, stderr, dirPath) => {
+				if (err)
+					return console.error(err);
+
+				var mipsRun: cp.ChildProcess = (new MipsRunner(path.join(dirPath, "proj.out"))).execute();
+				mipsRun.stdout.setEncoding('utf8');
+				mipsRun.stdout.on('data', (chunk) => {
+					socket.emit('stdout', chunk);
+				})
+			});
+		});
+	}
+}
