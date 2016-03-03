6b6ee91c7fe582d491fd2fdb9e605d939ac5f743
Jan Marker <jan@jangmarker.de>
2016-03-03 15:21:48 +0100

    Make sessions available in Socket.io
    if the user is not available within the session the
    user will be created with a random string as id
    usually the user should be provided by the login
    mechanism which is not in place yet

--- src/server/SessionManager.ts
+++ src/server/SessionManager.ts
@@ -1,5 +1,6 @@
 /// <reference path="../../typings/main.d.ts" />
 import {Session} from './Session'
+import * as randomstring from 'randomstring'
 
 export class SessionManager {
 	sessions: any = {};
@@ -11,7 +12,11 @@ export class SessionManager {
 	public startListening() {
 		this.io.on('connection', (socket: SocketIO.Socket) => {
 			//console.log(socket.request);
-			var userId = 'default';
+			if (!socket.handshake.session.user) {
+				socket.handshake.session.user = randomstring.generate(8);
+				socket.handshake.session.save();
+			}
+			var userId: string = socket.handshake.session.user;
 			if(!this.sessions[userId]) {
 				this.sessions[userId] = new Session();
 			}
