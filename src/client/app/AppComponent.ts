70345ac582fb28e02c3b0f907c6ae3cc618e0de8
fischerman <privat@bjorn-fischer.de>
2016-03-05 13:41:16 +0100

    Improve session communication between server and client

--- src/client/app/AppComponent.ts
+++ src/client/app/AppComponent.ts
@@ -14,21 +14,47 @@ declare var io: any;
 export class AppComponent {
 	private socket;
 	public stdout: string = "";
+	public gccErr: string = "";
 	public project: Project;
 	public selectedFile: File;
+	public running: boolean = false;
+	public connected: boolean = false;
 
 	constructor() {
-		this.socket = io.connect();
+		this.socket = io();
+		this.socket.on('connect', () => {
+			console.log("socket connected");
+			this.connected = true;
+		});
+		this.socket.on('disconnect', () => {
+			console.log("socket disconnected");
+			this.connected = false;
+		});
 		this.socket.on('stdout', (buffer) => {
 			this.stdout += buffer;
 		})
+		this.socket.on('exit', () => {
+			this.running = false;
+			console.log('exit');
+		});
+		this.socket.on('gcc-error', (err) => {
+			this.gccErr = err.toString();
+			console.log(this.gccErr);
+		});
 	    this.project = new Project("Test project");
 		this.selectedFile = this.project.files[0];
 	}
 
 	run() {
+		this.running = true;
+		this.stdout = "";
+		this.gccErr = "";
 		console.log("run %s", this.project);
 		this.socket.emit('run', this.project);
+	}
+
+	stop() {
+		this.socket.emit('stop');
 	}
 
 	selectFile(file: File) {
