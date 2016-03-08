bc2335a2de20d60dcdf26739e827adf48c36e420
Jan Marker <jan@jangmarker.de>
2016-03-08 11:32:29 +0100

    Fix typescript errors in Angular

--- src/client/app/main.ts
+++ src/client/app/main.ts
@@ -1,3 +1,4 @@
+/// <reference path="../../../node_modules/angular2/typings/browser.d.ts" />
 
 import {bootstrap}    from 'angular2/platform/browser'
 import {LeaComponent} from './lea/LeaComponent'
