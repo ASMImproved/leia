202b83a922d6896d52bea6b6898c9fd0b458b039
fischerman <privat@bjorn-fischer.de>
2016-03-19 14:14:25 +0100

    Add FileNameEndingService

---
+++ src/client/app/project/FileNameEndingService.spec.ts
@@ -0,0 +1,20 @@
+/// <reference path="../../../../typings/browser/ambient/jasmine/index.d.ts" />
+import {describe, it, expect} from 'angular2/testing';
+import {FileNameEndingService} from './FileNameEndingService'
+
+
+describe('FileNameEndingService', function() {
+    var fileNameEndingService: FileNameEndingService = new FileNameEndingService();
+
+    it('simple file name', function() {
+       expect(fileNameEndingService.getFileNameEnding('test.css')).toBe('css');
+    });
+
+    it('file name with two dots should only return the last bit', function() {
+        expect(fileNameEndingService.getFileNameEnding('test.a.b')).toBe('b');
+    });
+
+    it('file name without an ending should return NULL', function() {
+        expect(fileNameEndingService.getFileNameEnding('test')).toBeNull();
+    })
+});
