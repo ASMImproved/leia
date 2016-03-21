893d253a7e7419b289a7e886dc12ac69dc9cb440
fischerman <privat@bjorn-fischer.de>
2016-03-21 19:26:13 +0100

    Extend sample project to have multiple files

--- src/common/Project.ts
+++ src/common/Project.ts
@@ -5,26 +5,36 @@ export class Project {
 
 	constructor (public name: string) {
 		this.files = [];
-		var file: File = new File("main.s");
-		file.content = `
+		let main: File = new File("main.s");
+		main.content = `
+.text
+.globl main
+.extern hello
+
+main:
+  jal hello
+
+  li  $a0, 0
+  li  $v0, 4001
+  syscall
+`;
+		let hello: File = new File("hello.s");
+		hello.content = `
 .data
 hellostring:  .ascii "Hello "
               .asciiz "World!\\n"
 
 .text
-.globl main
-
-main:
+.globl hello
+hello:
   li  $a0, 1
   la  $a1, hellostring
   li  $a2, 14
   li  $v0, 4004
   syscall
-
-  li  $a0, 0
-  li  $v0, 4001
-  syscall
-`
-		this.files.push(file);
+  jr $ra
+`;
+		this.files.push(main);
+		this.files.push(hello);
 	}
 }
