abf63b17a2748f62201ed74c084e6d109e87530e
fischerman <privat@bjorn-fischer.de>
2016-04-01 16:46:54 +0200

    Fix false assignment and type memoryBlock

--- src/client/app/project/memory/MemoryService.ts
+++ src/client/app/project/memory/MemoryService.ts
@@ -3,18 +3,18 @@ import {Injectable, EventEmitter} from "angular2/core";
 import {SocketService} from "../SocketService";
 import {MemoryFrame} from "../../../../common/MemoryFrame";
 import {BehaviorSubject} from "rxjs/Rx";
+import {MemoryBlock} from "../../../../common/MemoryBlock";
 
 @Injectable()
 export class MemoryService {
     private _frame: MemoryFrame;
-    private _blocks;
+    private _blocks: BehaviorSubject<Array<MemoryBlock>>;
     public memoryBlocksChanged$;
 
     public constructor(private socketService: SocketService) {
-        this._blocks = new BehaviorSubject<any>([]);
+        this._blocks = new BehaviorSubject<Array<MemoryBlock>>([]);
         this.memoryBlocksChanged$ = this._blocks.asObservable();
-        this.socketService.socket.on('memoryUpdate', (blocks) => {
-            this._blocks = blocks;
+        this.socketService.socket.on('memoryUpdate', (blocks: Array<MemoryBlock>) => {
             console.log(blocks);
             this._blocks.next(blocks);
         });
