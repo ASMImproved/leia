a984368bf845b0266272adca59fd885228441d16
fischerman <privat@bjorn-fischer.de>
2016-04-05 16:37:15 +0200

    Improve ChangeMemoryCommand

--- src/server/ChangeMemoryFrameCommand.ts
+++ src/server/ChangeMemoryFrameCommand.ts
@@ -5,14 +5,22 @@ import {ExecutionContext} from "./command/ExecutionContext";
 
 export class ChangeMemoryFrameCommand implements ICommand{
 
-
-
     execute(payload: MemoryFrame, executionContext: ExecutionContext, callback:any) {
-        executionContext.memoryFrame = payload;
-        if(executionContext.mipsSession) {
-            let mips = executionContext.mipsSession;
-            if(mips.mipsProgram.debuggerStarted) {
-
+        executionContext.socketSession.memoryFrame = payload;
+        if(executionContext.socketSession.mipsSession) {
+            try {
+                let debug = executionContext.socketSession.mipsSession.mipsProgram.debug;
+                debug.readMemory("0x" + payload.start.toString(16), 10000)
+                .then((blocks: dbgmits.IMemoryBlock[]) => {
+                    callback(null, {}, [blocks]);
+                }, (err) => {
+                    console.log('read mem failed', err);
+                    callback(null, {
+                        memoryReadFailed: true
+                    }, []);
+                });
+            } catch(e) {
+                callback(e);
             }
         } else {
             callback(null, {}, []);
