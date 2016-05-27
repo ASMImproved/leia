163143e5211dc9e797d762dae6b07614ae99dabb
fischerman <privat@bjorn-fischer.de>
2016-05-27 11:58:32 +0200

    Implement notify method

--- src/client/app/project/notification/NotificationService.ts
+++ src/client/app/project/notification/NotificationService.ts
@@ -1,8 +1,15 @@
 
 import {Injectable} from "angular2/core";
 
+export enum NotificationLevel {
+    Info,
+    Warn,
+    Error
+}
+
 @Injectable()
 export class NotificationService {
+    private notificationsHistory: Array<{message: string, level: NotificationLevel, time}> = [];
 
     constructor() {
 
@@ -12,4 +19,14 @@ export class NotificationService {
         let answer = confirm(message);
         cb(null, answer);
     }
+
+    public notify(message: string, level: NotificationLevel) {
+        this.notificationsHistory.push({
+            message: message,
+            level: level,
+            time: new Date()
+        });
+        alert(level + ": " + message);
+    }
+
 }
